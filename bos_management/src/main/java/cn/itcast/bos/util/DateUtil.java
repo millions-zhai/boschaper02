6c829b070ad77bbbc9f23e2ad141cfcdb05acb5d
millions <millions_zhai@163.com>
2017-07-21 15:25:39 +0800

    datautil

---
+++ bos_management/src/main/java/cn/itcast/bos/util/DateUtil.java
@@ -0,0 +1,13 @@
+package cn.itcast.bos.util;
+
+import java.util.Date;
+
+/**
+ * Created by millions on 2017/7/21.
+ */
+public class DateUtil {
+
+    public static Date getCurrent(){
+        return new Date();
+    }
+}
